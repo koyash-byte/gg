0e8f0ddd71c527804f4eef838a55b54a67df0292
koyash-byte <134601141+koyash-byte@users.noreply.github.com>
2025-11-25 05:50:24 +0300

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -1,6 +1,6 @@
 AGE_ACCS = {
   --"emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3"
-
+  "emrekarseal", "emrekario"
 }
 RIO_ACCS = {
      --"emrekarseal", "emrekario", "emrekario2", "emrekario3", "emrekario4", "emrekario5", "emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3", "gngrio4" 
