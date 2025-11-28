8acd767e179552874b9351fec62ac9563b1305f2
koyash-byte <134601141+koyash-byte@users.noreply.github.com>
2025-11-28 15:49:12 +0300

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -6,7 +6,7 @@ RIO_ACCS = {
      "emrekario2", "emrekario3", "emrekario4", "emrekario5", "emrekario7" --"emrekarseal", "emrekario", "emrekario8", "gngrio1", "gngrio2", "gngrio3", "gngrio4" 
 }
 LITE_RIO_ACCS = {
-
+  "emrekario8", "gngrio1", "gngrio2", "gngrio3", "gngrio4"
 }
 WEIGHT_ACCS = {
 
