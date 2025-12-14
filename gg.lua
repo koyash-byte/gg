0f68c98084f99b22ce94ba2ce8f2206eaf7a02ae
koyash-byte <134601141+koyash-byte@users.noreply.github.com>
2025-12-14 22:54:50 +0300

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -5,7 +5,7 @@ PERU_ACCS = {
   "emrekarseal", "emrekario"
 }
 RIO_ACCS = {
-     "emrekario2", "emrekario3", "emrekario4", "emrekario5", "emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3", "gngrio4" 
+     "emrekario2", "emrekario3", "emrekario4", "emrekario5", "emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3", "gngrio4", "Gig4LionAc3", "XxSparklyLuckyHeroxX", "JulianRiftTV", "Blaz3Claw268", "WYAttHYper824", "Alph4Fusion", "Stormy_ShadowLOL", "XxG4l4xySp4rklyxX", "Rogue_VoidLOL", "SeBastianZer0FR0st"
 }
 LITE_RIO_ACCS = {
   
