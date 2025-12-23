ca3d1ca12a06aa0ddc4396bf50d1029b50fafada
koyash-byte <134601141+koyash-byte@users.noreply.github.com>
2025-12-23 21:59:44 +0300

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -2,7 +2,7 @@ AGE_ACCS = {
   --"emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3"
 }
 PERU_ACCS = {
-  "emrekarseal", "emrekario"
+  --"emrekarseal", "emrekario"
 }
 RIO_ACCS = {
      "emrekario2", "emrekario3", "emrekario4", "emrekario5", "emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3", "gngrio4", "Gig4LionAc3", "XxSparklyLuckyHeroxX", "JulianRiftTV", "Blaz3Claw268", "WYAttHYper824", "Alph4Fusion", "Stormy_ShadowLOL", "XxG4l4xySp4rklyxX", "Rogue_VoidLOL", "SeBastianZer0FR0st"
