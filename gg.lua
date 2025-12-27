6ff0426fdc476b38503a5e49e0da84bb3d2e0cd3
koyash-byte <134601141+koyash-byte@users.noreply.github.com>
2025-12-27 21:03:14 +0300

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -5,7 +5,7 @@ PERU_ACCS = {
   "emrekarseal", "emrekario"
 }
 RIO_ACCS = {
-     --"emrekario2", "emrekario3", "emrekario4", "emrekario5", "emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3", "gngrio4", "Gig4LionAc3", "XxSparklyLuckyHeroxX", "JulianRiftTV", "Blaz3Claw268", "WYAttHYper824", "Alph4Fusion", "Stormy_ShadowLOL", "XxG4l4xySp4rklyxX", "Rogue_VoidLOL", "SeBastianZer0FR0st", "FusionEchoBlock2014", "LightLion416", "GoldenViperNova", "EllIEP0wERRAVeN580", "CircuitKnightThunder", "OliverInferno428", "RavenPhoenixTV", "Xx_Pix3lH3roPix3l4t3", "Ezra_Playz943", "XxCr4ftCookiexX2012", "LightR4vEn", "LiamFusionPanda", "Gh0stChill76", "D4ncerG4l4xy2017", "VEnOmSHadoWBeaR20215", "Olivi4Wolf470", "XX_GrAyS0NPiXElStreA", "Ninj4QueenSt4r590", "SkyT0xicLight960", "LoganUltraWolf200991"
+     "emrekario2", "emrekario3", "emrekario4", "emrekario5", "emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3", "gngrio4", "Gig4LionAc3", "XxSparklyLuckyHeroxX", "JulianRiftTV", "Blaz3Claw268", "WYAttHYper824", "Alph4Fusion", "Stormy_ShadowLOL", "XxG4l4xySp4rklyxX", "Rogue_VoidLOL", "SeBastianZer0FR0st", "FusionEchoBlock2014", "LightLion416", "GoldenViperNova", "EllIEP0wERRAVeN580", "CircuitKnightThunder", "OliverInferno428", "RavenPhoenixTV", "Xx_Pix3lH3roPix3l4t3", "Ezra_Playz943", "XxCr4ftCookiexX2012", "LightR4vEn", "LiamFusionPanda", "Gh0stChill76", "D4ncerG4l4xy2017", "VEnOmSHadoWBeaR20215", "Olivi4Wolf470", "XX_GrAyS0NPiXElStreA", "Ninj4QueenSt4r590", "SkyT0xicLight960", "LoganUltraWolf200991"
 }
 LITE_RIO_ACCS = {
   
